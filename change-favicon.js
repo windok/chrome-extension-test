820657561e016fc42ac91acf50527a2bead017e9
Yevhenii <windok.ua@gmail.com>
2024-09-16 11:06:31 +0200

    change-favicon

---
+++ change-favicon.js
@@ -0,0 +1,30 @@
+const setIcon = () => {
+  const head = document.getElementsByTagName('head')[0];
+
+  let link;
+  do {
+    link = head.querySelector("link[rel~='icon']");
+    link?.remove()
+  } while (link);
+
+  link = document.createElement('link');
+  link.rel = 'icon';
+  head.appendChild(link);
+
+  const projectIcon = document.querySelector('#project-avatar')?.src;
+
+  const icons = {
+    'jira.internal.dnanexus.com': projectIcon ?? 'https://wac-cdn-bfldr.atlassian.com/K3MHR9G8/at/hmv84b686k38wqh7tpwrfkcv/jira-logo.svg',
+    'confluence.internal.dnanexus.com': 'https://wac-cdn-bfldr.atlassian.com/K3MHR9G8/at/wfr836ffrrh5b7qs43qhgkv/confluence-logo.svg'
+  }
+
+
+  for (const host of Object.keys(icons)) {
+    if (location.host.includes(host)) {
+      link.href = icons[host];
+    }
+  }
+};
+
+setIcon();
+setTimeout(setIcon, 2000);
